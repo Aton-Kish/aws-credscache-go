763398c830b1cfa65592a35516460fbf05adf84d
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2023-02-11 13:17:34 +0000

    move credscacheutil pkg

--- credscacheutil/cachekey.go
+++ credscacheutil/cachekey.go
@@ -18,7 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-package cachekey
+package credscacheutil
 
 type cachekey interface {
 	CacheKey() (string, error)
